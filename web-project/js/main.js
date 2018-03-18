0d8929aae66a55bcc4d60fad98b4a51ab87fbbf6
Mark Durrant <mark.j.durrant@gmail.com>
2018-03-18 12:24:58 +0000

    add splash

--- web-project/js/main.js
+++ web-project/js/main.js
@@ -46,3 +46,7 @@ function showPos(pos) {
 firebase.database().ref(' ').once('value').then(function(snapshot) {
   console.log(snapshot.val());
 });
+
+$('.cover').click(function() {
+  $(this).addClass('hide-me');
+});
